7b63d5544255df55e60086b3c70d1fe94a80e22d
Hamza Moumni <94747@supinfo.com>
2015-10-10 11:15:58 -0700

    Created the album model, artist model, review model and used scafolding to generate controllers and views

---
+++ MvsStore/MvsStore/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvsStore.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+        public int AlbumID { get; set; }
+        public virtual Album Album { get; set; }
+        public string Contents { get; set; }
+        [Required()]
+        [Display(Name = "Email Adress")]
+        [DataType(DataType.EmailAddress)]
+        public string ReviewerEmail { get; set; }
+    }
+}

f0a1f67dd46350e2131fbdba41d225a360d3fc01
Hamza Moumni <94747@supinfo.com>
2015-10-11 15:14:37 -0700

    fixed some bugs

---
+++ MvcStore/MvcStore/Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcStore.Models
+{
+   public class Album
+    {
+        public int AlbumID { get; set; }
+        public string Title { get; set; }
+        public Artist Artist { get; set; }
+        public virtual List<Review> Reviews { get; set; }
+    }
+}

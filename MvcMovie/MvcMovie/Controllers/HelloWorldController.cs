62a603ea66715ce27de0856b548f6ab95dbb14b9
Hamza Moumni <94747@supinfo.com>
2015-10-12 10:05:38 -0700

    Hello world controller

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "This is my <b> default </b> action";
+        }
+
+        public string details(string name, int id)
+        {
+            return HttpUtility.HtmlEncode("HEllo"+ name+" , id:"+id);
+        }
+    }
+}

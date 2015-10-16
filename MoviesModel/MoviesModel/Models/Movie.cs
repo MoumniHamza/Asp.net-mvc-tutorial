01108de57f561bdfab9f37e89c571644ba187006
Hamza Moumni <94747@supinfo.com>
2015-10-15 20:31:41 -0700

    Added some validation

--- MoviesModel/MoviesModel/Models/Movie.cs
+++ MoviesModel/MoviesModel/Models/Movie.cs
@@ -11,13 +11,17 @@ namespace MoviesModel.Models
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength =3)]
         public string Title { get; set; }
         [Display(Name ="Release Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         public string Rating { get; set; }
         public int Rank { get; set; }
 
